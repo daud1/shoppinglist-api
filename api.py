edcbf42e224628565fa5ab4e2fa79bf05cb56c2c
Daudi Mwebaza <daudi@Gus.local>
2017-11-18 18:23:36 +0300

    Switch to virtual machine. Update requirements.txt Update gitignore

--- api.py
+++ api.py
@@ -1,12 +1,13 @@
 """Monolithic module for flask shopping list api. includes multiple modules"""
 from flask_api import FlaskAPI
-from flask import render_template, url_for, request, redirect, jsonify
+from flask import jsonify
 from forms import LoginForm, SignUpForm, NewListForm, NewItemForm
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
 from flask_login import LoginManager, UserMixin, login_user
 from flask_login import logout_user, current_user, login_required
 from itsdangerous import (TimedJSONWebSignatureSerializer
-                           as Serializer, BadSignature, SignatureExpired)
+                          as Serializer, BadSignature, SignatureExpired)
 from flask_bcrypt import Bcrypt
 
 ######################### INIT ##########################
@@ -18,7 +19,9 @@ db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'not_really_secret'
 app.config['WTF_CSRF_ENABLED'] = False
 BCRYPT_LOG_ROUNDS = 12
+
 ####################### MODELS ####################################
+
 
 class User(db.Model, UserMixin):
     """This class represents the user table"""
@@ -31,11 +34,12 @@ class User(db.Model, UserMixin):
 
     def __init__(self, email, password):
         self.email = email
-        self.password = Bcrypt().generate_password_hash(password, BCRYPT_LOG_ROUNDS).decode()
-    
-    def generate_auth_token(self, expiration = 10800):
-        s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
-        return s.dumps({ 'id': self.id })
+        self.password = Bcrypt().generate_password_hash(
+            password, BCRYPT_LOG_ROUNDS).decode()
+
+    def generate_auth_token(self, expiration=10800):
+        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
 
     def __repr__(self):
         return '<Email %r>' % self.email
@@ -46,32 +50,35 @@ class User(db.Model, UserMixin):
         try:
             data = s.loads(token)
         except SignatureExpired:
-            return None # valid token, but expired
+            return None  # valid token, but expired
         except BadSignature:
-            return None # invalid token
+            return None  # invalid token
         user = User.query.get(data['id'])
         return user
-    
-    @property 
+
+    @property
     def serialize(self):
         """Return object data in easily serializeable format"""
-        return { 'email': self.email, 'password': self.password }
+        return {'email': self.email, 'password': self.password}
+
 
 class ShoppingList(db.Model):
     """This class represents the shopping_list table"""
     __tablename__ = 'shopping_list'
     id = db.Column(db.Integer, primary_key=True)
     list_name = db.Column(db.String(64), unique=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete='CASCADE'))
 
     def __init__(self, list_name, user_id):
         self.list_name = list_name
         self.user_id = user_id
-    
-    @property 
+
+    @property
     def serialize(self):
         """Return object data in easily serializeable format"""
-        return { 'list_name': self.list_name }
+        return {'list_name': self.list_name}
+
 
 class Item(db.Model):
     """This class represents the item table"""
@@ -79,46 +86,55 @@ class Item(db.Model):
     item_id = db.Column(db.Integer, primary_key=True)
     item_name = db.Column(db.String(32))
     quantity = db.Column(db.Integer)
-    list_id = db.Column(db.Integer, db.ForeignKey('shopping_list.id', ondelete='CASCADE'), nullable=False)
+    list_id = db.Column(db.Integer, db.ForeignKey(
+        'shopping_list.id', ondelete='CASCADE'), nullable=False)
 
     def __init__(self, item_name, list_id, quantity=1):
         self.item_name = item_name
         self.list_id = list_id
         self.quantity = quantity
-    
-    @property 
+
+    @property
     def serialize(self):
         """Return object data in easily serializeable format"""
-        return { 'item_name': self.list_name, 'list_id': self.list_id }
+        return {'item_name': self.list_name, 'list_id': self.list_id}
 
 ####################### LOGIN & LOGOUT ############################
+
 
 LOGIN_MANAGER = LoginManager()
 LOGIN_MANAGER.init_app(app)
 LOGIN_MANAGER.login_view = 'login'
 
+
 @LOGIN_MANAGER.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 @app.route('/auth/register', methods=['POST'])
 def register():
     form = SignUpForm()
     if form.validate_on_submit():
-        #check that the email is unique
         usr = User(str(request.form['email']), str(request.form['password']))
         if usr:
-            db.session.add(usr)
-            db.session.commit()
-            response = jsonify({'MSG':'Success'})
+            try:
+                db.session.add(usr)
+                db.session.commit()
+            except exc.IntegrityError:
+                response = jsonify(
+                    {'ERR': 'User email already exists, please choose another'})
+
+            response = jsonify({'MSG': 'Success'})
             response.status_code = 201
         else:
-            response = jsonify({'ERR':'User wasn\'t created.'})
+            response = jsonify({'ERR': 'User wasn\'t created.'})
             response.status_code = 400
     else:
         response = jsonify({'ERR': form.errors})
         response.status_code = 400
     return response
+
 
 @app.route('/auth/login', methods=['POST'])
 def login():
@@ -131,15 +147,16 @@ def login():
             usr.token = tkn
             db.session.commit()
             login_user(usr)
-            response = jsonify({'MSG':'Login Successful', 'token': tkn})
+            response = jsonify({'MSG': 'Login Successful', 'token': tkn})
             response.status_code = 200
         else:
-            response = jsonify({'ERR':'Incorrect Password'})
+            response = jsonify({'ERR': 'Incorrect Password'})
             response.status_code = 401
     else:
         response = jsonify({'ERR': 'User does not exist'})
         response.status_code = 404
     return response
+
 
 @app.route('/auth/logout', methods=['POST'])
 @login_required
@@ -154,6 +171,7 @@ def logout():
 
 ###################### ROUTES ##################
 
+
 @app.route('/shoppinglists', methods=['GET'])
 @login_required
 def view_all_lists():
@@ -162,14 +180,15 @@ def view_all_lists():
         response = jsonify([obj.serialize for obj in all_sh_lists])
         response.status_code = 200
     else:
-        response = jsonify({'ERR':'No lists returned.'})
+        response = jsonify({'ERR': 'No lists returned.'})
         response.status_code = 404
     return response
+
 
 @app.route('/shoppinglists', methods=['POST'])
 @login_required
 def create_list():
-    form = NewListForm()   
+    form = NewListForm()
     new_list = ShoppingList(form.list_name.data, current_user.get_id())
     if new_list is not None and form.validate_on_submit():
         db.session.add(new_list)
@@ -177,9 +196,10 @@ def create_list():
         response = jsonify({'MSG': 'Success'})
         response.status_code = 201
     else:
-        response = jsonify({'ERR':'List was not created'})                
+        response = jsonify({'ERR': 'List was not created'})
         response.status_code = 400
     return response
+
 
 @app.route('/shoppinglists/<id>', methods=['PUT'])
 @login_required
@@ -190,17 +210,18 @@ def edit_list(id):
         if form.validate_on_submit():
             ed_list.list_name = form.list_name.data
             db.session.commit()
-            response = jsonify({'MSG':'Success'})
+            response = jsonify({'MSG': 'Success'})
             response.status_code = 201
         else:
             response = jsonify({'ERR': form.errors})
     else:
-        response = jsonify({'ERR':'failed to find list'})
+        response = jsonify({'ERR': 'failed to find list'})
         response.status_code = 404
-    
+
     return response
 
-@app.route('/shoppinglists/<id>', methods = ['DELETE'])
+
+@app.route('/shoppinglists/<id>', methods=['DELETE'])
 @login_required
 def delete_list(id):
     del_list = ShoppingList.query.filter_by(id=id).first()
@@ -213,12 +234,13 @@ def delete_list(id):
                 db.session.delete(del_items)
 
         db.session.commit()
-        response = jsonify({'MSG':'Success'})
+        response = jsonify({'MSG': 'Success'})
         response.status_code = 204
     else:
-        response = jsonify({'ERR':'Requested list was not found'})
+        response = jsonify({'ERR': 'Requested list was not found'})
         response.status_code = 404
     return response
+
 
 @app.route('/shoppinglists/<id>', methods=['GET'])
 @login_required
@@ -228,9 +250,10 @@ def view_list(id):
         response = jsonify(list_items)
         response.status_code = 200
     else:
-        response = jsonify({'ERR':'List items not Found'})
+        response = jsonify({'ERR': 'List items not Found'})
         response.status_code = 400
     return response
+
 
 @app.route('/shoppinglists/<id>/items/', methods=['POST'])
 @login_required
@@ -247,14 +270,15 @@ def add_item(id):
         response = jsonify({'MSG': 'Item added to list'})
         response.status_code = 201
     else:
-        response.jsonify({'ERR':'Item wasnt added to list'})
+        response.jsonify({'ERR': 'Item wasnt added to list'})
         response.status_code = 400
     return response
+
 
 @app.route('/shoppinglists/<id>/items/<item_id>', methods=['PUT'])
 @login_required
 def edit_item(id, item_id):
-    form = NewItemForm() 
+    form = NewItemForm()
     ed_item = Item.query.filter_by(list_id=id, item_id=item_id).first()
     if form.validate_on_submit():
         if form.item_name.data:
@@ -266,13 +290,14 @@ def edit_item(id, item_id):
         if form.quantity.data and form.item_name.data:
             ed_item.item_name = form.item_name.data
             ed_item.quantity = form.quantity.data
-        
-        response = jsonify({'MSG':'Edited item.'})
+
+        response = jsonify({'MSG': 'Edited item.'})
         response.status_code = 200
     else:
-        response = jsonify({'MSG':form.errors})
+        response = jsonify({'MSG': form.errors})
         response.status_code = 404
     return response
+
 
 @app.route('/shoppinglists/<id>/items/<item_id>', methods=['DELETE'])
 @login_required
@@ -281,12 +306,13 @@ def delete_item(id, item_id):
     if del_item is not None:
         db.session.delete(del_item)
         db.session.commit()
-        response = jsonify({'MSG':'Success'})
+        response = jsonify({'MSG': 'Success'})
         response.status_code = 204
     else:
-        response = jsonify({'ERR':'Requested item was not found'})
+        response = jsonify({'ERR': 'Requested item was not found'})
         response.status_code = 404
-    return response 
+    return response
+
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------
 db.create_all()
--- forms.py
+++ forms.py
@@ -1,19 +1,35 @@
+"""module for shoppinglist-api forms"""
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, EqualTo, Email
 from wtforms import TextField, PasswordField, IntegerField, StringField
 
+
 class LoginForm(FlaskForm):
-    email = TextField('Email Address', validators=[DataRequired(message='Please enter an email address')])
-    password = PasswordField('Enter Password', validators=[DataRequired('Please enter your password')])
+    """class to represent the login form"""
+    email = TextField('Email Address', validators=[
+        DataRequired(message='Please enter an email address')])
+    password = PasswordField('Enter Password', validators=[
+        DataRequired('Please enter your password')])
+
 
 class SignUpForm(FlaskForm):
-    email = TextField('Email Address', validators=[DataRequired(message='Enter your email address'), Email('Invalid Email')])
-    password = PasswordField('Password', validators=[DataRequired(message='Enter a password.'), EqualTo('confirm')])
-    confirm = PasswordField('Confirm Password', validators=[DataRequired(message="Type your password again.")])
+    """class to represent the sign-up form"""
+    email = TextField('Email Address', validators=[DataRequired(
+        message='Enter your email address'), Email('Invalid Email')])
+    password = PasswordField('Password', validators=[DataRequired(
+        message='Enter a password.'), EqualTo('confirm')])
+    confirm = PasswordField('Confirm Password', validators=[DataRequired(
+        message="Type your password again.")])
+
 
 class NewListForm(FlaskForm):
-    list_name = StringField('Title', validators=[DataRequired(message='Enter a title for your shopping list')])
+    """class to represent the new-list form"""
+    list_name = StringField('Title', validators=[DataRequired(
+        message='Enter a title for your shopping list')])
+
 
 class NewItemForm(FlaskForm):
-    item_name = TextField('Item Name', validators=[DataRequired('Enter a name for this item')])
+    """class to represent the new-item form"""
+    item_name = TextField('Item Name', validators=[
+        DataRequired('Enter a name for this item')])
     quantity = IntegerField('Quantity')
